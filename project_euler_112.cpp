6cceb3f2a2c276bea5fb8a84ef91e672f60e85e9
Kirill Temlyakov <Kirill@simplerelevance.com>
2013-09-07 16:33:01 -0500

    problem_112 solved

---
+++ project_euler_112.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+
+using namespace::std;
+
+bool isBouncy(int x);
+double getAnswer(double cutoff);
+
+int main()
+{
+  cout <<"I am in project euler 112"<<endl;
+
+  double test = 0.0;
+
+  test = getAnswer(0.99);
+
+  cout <<"answer : " << (int)test << endl;
+
+  return 0;
+}
+
+bool isBouncy(int x)
+{
+  int temp = 0;
+  bool increased=false, decreased=false;
+
+  int prev = x % 10;
+  x = x / 10;
+
+  while (x > 0)
+    {
+      temp = x % 10;
+      if(temp > prev)
+	{
+	  increased = true;
+	}
+      if(temp < prev)
+	{
+	  decreased = true;
+	}
+      if (increased == true && decreased == true)
+	{
+	  return true;
+	}
+      prev = temp;
+      x = x / 10;
+    }
+  return false;
+}
+
+double getAnswer(double cutoff)
+{
+  double bouncy_count=0, ratio=0;
+  int counter=0;
+  while(ratio < cutoff)
+    {
+      counter++;
+      if (isBouncy(counter) == true)
+	{
+	  bouncy_count++;
+	}
+      ratio = bouncy_count / (double)counter;
+      if (counter % 10000 == 0)
+	{
+	  cout << "working on" << counter <<endl;
+	  cout << "ratio" << ratio << endl;
+	}
+    }
+  return counter;
+}
+
